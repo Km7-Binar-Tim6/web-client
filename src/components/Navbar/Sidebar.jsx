93ee6328d6a40da93ba3ce40bd545a4c3119605d
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-11-12 18:38:32 +0700

    add feature transmission

--- src/components/Navbar/Sidebar.jsx
+++ src/components/Navbar/Sidebar.jsx
@@ -1,61 +1,56 @@
-import { Link } from "@tanstack/react-router";
-import { FaList } from "react-icons/fa";
-import PropTypes from "prop-types";
+import { Link } from '@tanstack/react-router';
+import { FaList } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 
 const Sidebar = ({ sidebarOpen }) => {
-  return (
-    <aside
-      id="sidebar"
-      className={`sidebar ${sidebarOpen ? "open" : ""} d-flex flex-column`}
-    >
-      <div className="h-100 sidebar-content">
-        <div className="sidebar-logo p-3">
-          <Link to="/" className="fw-bold fs-5">
-            Students
-          </Link>
-        </div>
-        <ul className="nav flex-column sidebar-nav p-0">
-          <li className="sidebar-header p-3 small">Admin Elements</li>
-          <li className="sidebar-item">
-            <Link
-              to="/"
-              className="nav-link sidebar-link d-flex align-items-center"
-            >
-              <FaList className="me-2" /> Dashboard
-            </Link>
-          </li>
-          <li className="sidebar-item">
-            <Link
-              to="/students"
-              className="nav-link sidebar-link d-flex align-items-center"
-            >
-              <FaList className="me-2" /> StudentsList
-            </Link>
-          </li>
-          <li className="sidebar-item">
-            <Link
-              to="/manufacture"
-              className="nav-link sidebar-link d-flex align-items-center"
-            >
-              <FaList className="me-2" /> Manufacture
-            </Link>
-          </li>
-          <li className="sidebar-item">
-            <Link
-              to="/type"
-              className="nav-link sidebar-link d-flex align-items-center"
-            >
-              <FaList className="me-2" /> Type
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </aside>
-  );
+	return (
+		<aside id="sidebar" className={`sidebar ${sidebarOpen ? 'open' : ''} d-flex flex-column`}>
+			<div className="h-100 sidebar-content">
+				<div className="sidebar-logo p-3">
+					<Link to="/" className="fw-bold fs-5">
+						Students
+					</Link>
+				</div>
+				<ul className="nav flex-column sidebar-nav p-0">
+					<li className="sidebar-header p-3 small">Admin Elements</li>
+					<li className="sidebar-item">
+						<Link to="/" className="nav-link sidebar-link d-flex align-items-center">
+							<FaList className="me-2" /> Dashboard
+						</Link>
+					</li>
+					<li className="sidebar-item">
+						<Link to="/students" className="nav-link sidebar-link d-flex align-items-center">
+							<FaList className="me-2" /> StudentsList
+						</Link>
+					</li>
+					<li className="sidebar-item">
+						<Link to="/manufacture" className="nav-link sidebar-link d-flex align-items-center">
+							<FaList className="me-2" /> Manufacture
+						</Link>
+					</li>
+					<li className="sidebar-item">
+						<Link to="/transmission" className="nav-link sidebar-link d-flex align-items-center">
+							<FaList className="me-2" /> Transmission
+						</Link>
+					</li>
+					<li className="sidebar-item">
+						<Link to="/type" className="nav-link sidebar-link d-flex align-items-center">
+							<FaList className="me-2" /> Type
+						</Link>
+					</li>
+					<li className="sidebar-item">
+						<Link to="/caroptions" className="nav-link sidebar-link d-flex align-items-center">
+							<FaList className="me-2" /> Car Options
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</aside>
+	);
 };
 
 Sidebar.propTypes = {
-  sidebarOpen: PropTypes.bool.isRequired,
+	sidebarOpen: PropTypes.bool.isRequired,
 };
 
 export default Sidebar;
