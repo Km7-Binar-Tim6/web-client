ba414f4774559b2c507b81563df9f0fe00f5bc84
casestudyprogramming <casestudyprogramming@gmail.com>
2024-11-13 16:11:30 +0000

    test ci/cd gcp

--- src/routes/index.lazy.jsx
+++ src/routes/index.lazy.jsx
@@ -7,7 +7,7 @@ export const Route = createLazyFileRoute("/")({
 function Index() {
   return (
     <div className="mt-4">
-      <h1>Ini adalah dashboard utama silahkan menggunakan sidebar</h1>
+      <h1>Ini adalah dashboard utama silahkan menggunakan sidebar untuk akses feature</h1>
     </div>
   );
 }
