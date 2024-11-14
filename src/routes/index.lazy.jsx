f272df7e6ea54146131f0cc1851d855757b53ef4
casestudyprogramming <casestudyprogramming@gmail.com>
2024-11-14 13:42:36 +0000

    coba

--- src/routes/index.lazy.jsx
+++ src/routes/index.lazy.jsx
@@ -7,7 +7,7 @@ export const Route = createLazyFileRoute("/")({
 function Index() {
   return (
     <div className="mt-4">
-      <h1>Ini adalah dashboard utama silahkan menggunakan sidebar untuk akses feature cars, manufacture, transmission, dll</h1>
+      <h1>Ini adalah dashboard utama silahkan menggunakan sidebar untuk akses feature cars, manufacture, transmission, dll apa saja</h1>
     </div>
   );
 }
